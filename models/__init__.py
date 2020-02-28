611e9ec94a524d9b60fcaa417314067681615a4e
fv316 <fv316@ic.ac.uk>
2020-02-28 16:49:14 +0100

    adding resnet1d model and update readme

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,8 @@ from models.resnet import resnet
 from models.squeezenet import squeezenet
 from models.lenet import lenet
 from models.cnn1d import cnn1d
+from models.resnet1d import resnet1d
+from models.resnet1d_v2 import resnet1d_v2
 
 
 def get_model(args):
@@ -16,6 +18,10 @@ def get_model(args):
         model = model_instance(args.model_name, args.num_classes, args.input_channels)
     elif args.arch == 'cnn1d':
         model = model_instance(args.model_name, args.num_classes, args.input_channels)
+    elif args.arch == 'resnet1d':
+        model = model_instance(args.model_name, args.num_classes, args.input_channels)
+    elif args.arch == 'resnet1d_v2':
+        model = model_instance(args.model_name, args.num_classes, args.input_channels)
     else:
         raise 'Model {} not available'.format(args.arch)
 
@@ -26,5 +32,7 @@ def _get_model_instance(name):
         'resnet': resnet,
         'squeezenet': squeezenet,
         'lenet': lenet,
-        'cnn1d': cnn1d
+        'cnn1d': cnn1d,
+        'resnet1d': resnet1d,
+        'resnet1d_v2': resnet1d_v2
     }[name]
