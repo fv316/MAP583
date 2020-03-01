87bcd2156e9184ae7f93c947a678d7957ae09386
Szymon Pajzert <szymonpajzert@gmail.com>
2020-03-01 23:31:18 +0100

    Updated requirements

--- test_running.sh
+++ test_running.sh
@@ -6,16 +6,22 @@ function test_model() {
     arch=$1
     model_name=$2
 
-    python commander.py \
-        --dataset ecg   \
-        --name ecg_${arch}_${model_name}_optsgd_lr1e-2_lrStep0.5_bsz128 \
-        --num-classes 5 \
-        --epochs 5 \
-        --root-dir ecg_data \
-        --arch ${arch} \
-        --model-name ${model_name} \
-        --batch-size 128 \
-        --short-run
+    echo "Testing arch $1 $2 "
+    for other_flags in "" "--sampler equal" "--sampler importance" "--class-balance equal" "--class-balance importance"; do
+        python commander.py \
+            --dataset ecg   \
+            --name ecg_${arch}_${model_name}_optsgd_lr1e-2_lrStep0.5_bsz128 \
+            --num-classes 5 \
+            --epochs 3 \
+            --root-dir ecg_data \
+            --arch ${arch} \
+            --model-name ${model_name} \
+            --batch-size 128 \
+            --short-run \
+            ${other_flags} &> /dev/null
+
+        echo "done"
+    done
 }
 
 test_model cnn1d cnn1d_3
